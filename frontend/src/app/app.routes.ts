130ac21e0de0e6f8e7734e8218d9079792de7d68
Jonathan Huaman <jonathan.huaman18@gmail.com>
2026-06-21 14:54:50 -0500

    Agregar pagina tienda JONZKO

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,13 +3,20 @@ import { Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin/admin-login.component';
 import { AdminComponent } from './admin/admin.component';
 import { ProductDetail } from './product-detail/product-detail';
+
 import { RegisterComponent } from './pages/register/register';
 import { LoginComponent } from './pages/register/login/login';
 import { MisPedidosComponent } from './pages/register/mis-pedidos/mis-pedidos';
 import { CheckoutComponent } from './pages/register/checkout/checkout';
 import { ResetPasswordComponent } from './pages/register/reset-password';
 
+import { TiendaComponent } from './pages/register/tienda/tienda';
+
 export const routes: Routes = [
+  {
+    path: 'tienda',
+    component: TiendaComponent
+  },
   {
     path: 'producto/:id',
     component: ProductDetail
